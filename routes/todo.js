81e82952f39baeee1bccb766d6b70a24574faa28
kravchuks <kravchuk.slv@rambler.ru>
2023-10-24 17:45:50 -0500

    Add todo deleting

--- routes/todo.js
+++ routes/todo.js
@@ -45,6 +45,22 @@ router.put("/:id", async (req, res) => {
 });
 
 // delete a todo
-router.delete("/:id", (req, res) => {});
+router.delete("/:id", async (req, res) => {
+  try {
+    const todos = await Todo.findAll({
+      where: {
+        id: +req.params.id,
+      },
+    });
+    const todo = todos[0];
+    await todo.destroy();
+    res.status(204).json({});
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      message: "Server error",
+    });
+  }
+});
 
 module.exports = router;
